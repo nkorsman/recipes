d2943ab1a7810147d6b759c0cfa1169ab30f39a8
Nikolas Korsman <nikolas@iki.fi>
2026-03-26 17:58:05 +0200

    Moved login to own page and improved templates

--- app.py
+++ app.py
@@ -21,8 +21,11 @@ def register():
     return render_template("register.html")
 
 
-@app.route("/login", methods=["POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
     username = request.form["username"]
     password = request.form["password"]
 
