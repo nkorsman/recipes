1371c05fb880bcfc013f4c247d5e1e2c5c62d543
Nikolas Korsman <nikolas@iki.fi>
2026-05-02 13:59:39 +0300

    seed.py now generates all types of objects stored in database

--- seed.py
+++ seed.py
@@ -1,15 +1,35 @@
 import sqlite3
-from random import choices, randint
+from random import choices, randint, sample
 from string import ascii_letters
+
+user_count = 10_000
+tag_count = 10_000
+recipe_count = 100_000
 
 db = sqlite3.connect("database.db")
 db.execute("PRAGMA foreign_keys = ON")
 
 
+def generate_users():
+    for i in range(user_count):
+        username = "".join(choices(ascii_letters, k=randint(5, 20)))
+        sql = "INSERT INTO Users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, "abc"])
+    db.commit()
+
+
+def generate_tags():
+    for i in range(tag_count):
+        tag_name = "".join(choices(ascii_letters, k=randint(4, 10)))
+        sql = "INSERT INTO Tags (name) VALUES (?)"
+        db.execute(sql, [tag_name])
+    db.commit()
+
+
 def generate_recipes():
-    for i in range(100):
-        title = f"Recipe {i}"
-        author_id = 1
+    for i in range(recipe_count):
+        title = "".join(choices(ascii_letters, k=randint(15, 50)))
+        author_id = randint(1, user_count)
 
         sql = """INSERT INTO Recipes (title, author_id, created_at, updated_at, is_draft)
                 VALUES (?, ?, DATETIME('now'), DATETIME('now'), 0)"""
@@ -30,24 +50,43 @@ def generate_recipes():
                     VALUES (?, ?, ?)"""
             db.execute(sql, [random_string, j, recipe_id])
 
-        db.commit()
+        for j in range(randint(0, 6)):
+            tag_id = randint(1, tag_count)
+            sql = "INSERT INTO RecipeTags (recipe_id, tag_id) VAlUES (?, ?)"
+            try:
+                db.execute(sql, [recipe_id, tag_id])
+            except sqlite3.IntegrityError:
+                # recipe might already have this tag
+                pass
 
-
-def generate_reviews():
-    recipe_id = 1
-    for i in range(1000):
-        username = "".join(choices(ascii_letters, k=randint(5, 20)))
-        sql = "INSERT INTO Users (username, password_hash) VALUES (?, ?)"
-        result = db.execute(sql, [username, "abc"])
-        user_id = result.lastrowid
-
-        content = "".join(choices(ascii_letters, k=randint(10, 150)))
-        rating = randint(1, 5)
-
-        sql = """INSERT INTO Reviews (recipe_id, user_id, rating, content, created_at, updated_at)
-                VALUES (?, ?, ?, ?, DATETIME('now'), DATETIME('now'))"""
-        db.execute(sql, [recipe_id, user_id, rating, content])
     db.commit()
 
 
+def generate_reviews():
+    for i in range(1, recipe_count + 1):
+        count = randint(1, 100)
+        user_ids = sample(range(1, user_count + 1), count)
+        for j in user_ids:
+            content = "".join(choices(ascii_letters, k=randint(10, 150)))
+            rating = randint(1, 5)
+            sql = """INSERT INTO Reviews (recipe_id, user_id, rating, content, created_at, updated_at)
+                    VALUES (?, ?, ?, ?, DATETIME('now'), DATETIME('now'))"""
+            db.execute(sql, [i, j, rating, content])
+    db.commit()
+
+
+def generate_favorites():
+    for i in range(1, recipe_count + 1):
+        count = randint(1, 100)
+        user_ids = sample(range(1, user_count + 1), count)
+        for j in user_ids:
+            sql = "INSERT INTO UserFavorites (user_id, recipe_id) VALUES (?, ?)"
+            db.execute(sql, [j, i])
+    db.commit()
+
+
+generate_users()
+generate_tags()
+generate_recipes()
 generate_reviews()
+generate_favorites()
